d74111bcaca1dd0ae87541ed26644235c518e6c1
Bryce Boe <bbzbryce@gmail.com>
2018-04-14 14:50:04 -0700

    Store state for challenges in ChallengeSection

--- scoreboard_frontend/src/challenge.jsx
+++ scoreboard_frontend/src/challenge.jsx
@@ -1,52 +1,44 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-class Challenge extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      category: 'Web',
-      points: 220,
-      solvedBy: 0,
-      title: 'Easypisy',
-    };
+function Challenge(props) {
+  const {
+    category, points, onClick, solvedBy, title,
+  } = props;
+  let className = 'menu-header menu-header-solved';
+  let status;
+  if (solvedBy > 1) {
+    status = `(Ordered by ${solvedBy} teams)`;
+  } else if (solvedBy === 1) {
+    status = '(Ordered by 1 team)';
+  } else {
+    className = 'menu-header';
+    status = '(Be the first one to order it)';
   }
 
-  handleClick() {
-    this.setState({ solvedBy: (this.state.solvedBy + 1) % 3 });
-  }
-
-  render() {
-    const {
-      category, points, solvedBy, title,
-    } = this.state;
-    let className = 'menu-header menu-header-solved';
-    let status;
-    if (solvedBy > 1) {
-      status = `(Ordered by ${solvedBy} teams)`;
-    } else if (solvedBy === 1) {
-      status = '(Ordered by 1 team)';
-    } else {
-      className = 'menu-header';
-      status = '(Be the first one to order it)';
-    }
-
-    return (
-      <div
-        className="menu-item"
-        onClick={() => this.handleClick()}
-        onKeyPress={() => {}}
-        role="presentation"
-      >
-        <h3 className={className}>{title}</h3>
-        <div className="menu-box">
-          <div className="menu-text">
-            {category}<br />
-            {status}
-          </div>
-          <span className="menu-points">{points}pt</span>
+  return (
+    <div
+      className="menu-item"
+      onClick={onClick}
+      onKeyPress={() => {}}
+      role="presentation"
+    >
+      <h3 className={className}>{title}</h3>
+      <div className="menu-box">
+        <div className="menu-text">
+          {category}<br />
+          {status}
         </div>
+        <span className="menu-points">{points}pt</span>
       </div>
-    );
-  }
+    </div>
+  );
 }
+Challenge.propTypes = {
+  category: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  points: PropTypes.number.isRequired,
+  solvedBy: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+};
 export default Challenge;
--- scoreboard_frontend/src/challenge_section.jsx
+++ scoreboard_frontend/src/challenge_section.jsx
@@ -4,15 +4,50 @@ import Challenge from './challenge';
 class ChallengeSection extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: 'Amouse Bouche' };
+    this.state = {
+      challenges: [
+        {
+          category: 'Web',
+          points: 220,
+          solvedBy: 45,
+          title: 'Easypisy',
+        },
+        {
+          category: 'Shellcoding',
+          points: 500,
+          solvedBy: 0,
+          title: 'Binary Sunny-Side-Up',
+        },
+        {
+          category: 'Shellcoding',
+          points: 130,
+          solvedBy: 1,
+          title: 'Technical Support',
+        },
+      ],
+      title: 'Amouse Bouche',
+    };
   }
+
+  handleClick(index) {
+    const challenges = this.state.challenges.slice();
+    challenges[index].solvedBy = (challenges[index].solvedBy + 1) % 3;
+    this.setState({ challenges });
+  }
+
   render() {
+    const challenges = this.state.challenges.map((challenge, index) =>
+      (
+        <Challenge
+          {...challenge}
+          key={challenge.title}
+          onClick={() => this.handleClick(index)}
+        />
+      ));
     return (
       <div>
         <h2 className="section-title">{this.state.title}</h2>
-        <Challenge />
-        <Challenge />
-        <Challenge />
+        {challenges}
       </div>
     );
   }
