e7c939d820b77b67197b7fa26bc53a8d045ed66a
Bryce Boe <bbzbryce@gmail.com>
2018-04-14 10:14:00 -0700

    Add react frontend with simple static challenge component

---
+++ scoreboard_frontend/src/challenge.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Challenge() {
+  return (
+    <div>
+      <h3><span aria-label="Sparkles" role="img">✨</span>Easypisy</h3>
+      <div>
+        <div>Web</div>
+        <div>(Be the first one to order it)</div>
+      </div>
+      <div>220pt</div>
+    </div>
+  );
+}
+export default Challenge;
---
+++ scoreboard_frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Challenge from './challenge';
+
+ReactDOM.render(
+  React.createElement(Challenge),
+  document.getElementById('root'),
+);
