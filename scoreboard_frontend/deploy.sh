455cd7511b58f29dfbb9f76d9a2b699fe628d2a9
Bryce Boe <bbzbryce@gmail.com>
2018-04-15 19:51:34 -0700

    Distinguish deploy script from build script

--- scoreboard_frontend/deploy.sh
+++ scoreboard_frontend/deploy.sh
@@ -3,4 +3,5 @@
 ./node_modules/.bin/react-scripts build \
   && rm build/static/*/*.map \
   && find build/static -regex '.*\.[cj]ss*' -exec sed -i '' '/^\/[/*]# sourceMappingURL/ d' {} \; \
-  && aws s3 sync --profile ooo build/ s3://oooverflow-scoreboard
+  && aws --profile ooo s3 cp ./build/index.html s3://oooverflow-scoreboard/index.html --cache-control max-age=60 \
+  && aws --profile ooo s3 sync build/ s3://oooverflow-scoreboard
