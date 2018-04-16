4fa0a471a0c8baae616ee4004a41c76774a018ab
Bryce Boe <bbzbryce@gmail.com>
2018-04-16 11:02:26 -0700

    Deploy to the scoreboard bucket prefix

--- scoreboard_frontend/deploy.sh
+++ scoreboard_frontend/deploy.sh
@@ -2,6 +2,8 @@
 
 ./node_modules/.bin/react-scripts build \
   && rm build/static/*/*.map \
+  && rm build/asset-manifest.json \
+  && rm build/service-worker.js \
   && find build/static -regex '.*\.[cj]ss*' -exec sed -i '' '/^\/[/*]# sourceMappingURL/ d' {} \; \
-  && aws --profile ooo s3 cp ./build/index.html s3://oooverflow-scoreboard/index.html --cache-control max-age=60 \
-  && aws --profile ooo s3 sync build/ s3://oooverflow-scoreboard
+  && aws --profile ooo s3 cp ./build/index.html s3://oooverflow-scoreboard/scoreboard/index.html --cache-control max-age=60 \
+  && aws --profile ooo s3 sync build/ s3://oooverflow-scoreboard/scoreboard
