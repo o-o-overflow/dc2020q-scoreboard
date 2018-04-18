7854c0277f2c78c4c2ce49644fc31513918809fa
Bryce Boe <bbzbryce@gmail.com>
2018-04-17 23:20:54 -0700

    Configure lambda to communicate with RDS using invalid password

--- scoreboard_backend/handler.py
+++ scoreboard_backend/handler.py
@@ -1,9 +1,15 @@
 import json
 
+import psycopg2
+
+DB_HOST = 'scoreboard-dev.cgwgx6ftjwg2.us-east-2.rds.amazonaws.com'
+DB_PASSWORD = 'INVALID'
+
 
 def hello(event, context):
+    psql = psycopg2.connect(dbname='scoreboard', host=DB_HOST,
+                            password=DB_PASSWORD, user='scoreboard')
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
 
@@ -13,12 +19,3 @@ def hello(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
