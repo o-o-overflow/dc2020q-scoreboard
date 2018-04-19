6e4b41f806c84ba13980b6bef38a6172961a8107
Bryce Boe <bbzbryce@gmail.com>
2018-04-19 14:12:12 -0700

    Add initial migration code

--- scoreboard_backend/handler.py
+++ scoreboard_backend/handler.py
@@ -1,8 +1,12 @@
 import base64
+import logging
 import os
 
 import boto3
 import psycopg2
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
 
 
 def kms_decrypt(b64data):
@@ -12,14 +16,38 @@ def kms_decrypt(b64data):
     return kms.decrypt(CiphertextBlob=data)['Plaintext'].decode('utf-8')
 
 
+def latest_migration():
+    with PSQL.cursor() as cursor:
+        LOGGER.info('Create schema_migration if necessary')
+        cursor.execute("""CREATE TABLE IF NOT EXISTS schema_migrations (
+id           integer PRIMARY KEY,
+date_applied date NOT NULL
+);""")
+        LOGGER.info("Find latest migration")
+        cursor.execute("""SELECT id from schema_migrations
+ORDER BY id DESC
+LIMIT 1;""")
+        return cursor.fetchone()
+
+
 PSQL = psycopg2.connect(dbname='scoreboard', host=os.getenv('DB_HOST'),
                         password=kms_decrypt(os.getenv('DB_PASSWORD')),
                         user='scoreboard')
 
 
-def hello(event, context):
+def migrate(_event, _context):
+    result = latest_migration()
+    LOGGER.info('COMMIT')
+    PSQL.commit()
+    return {
+        "statusCode": 200,
+        "body": {"result": result}
+    }
+
+
+def hello(_event, _context):
     with PSQL.cursor() as cursor:
-        cursor.execute("SELECT * FROM pg_catalog.pg_tables;")
+        cursor.execute("SELECT * from schema_migrations;")
         result = cursor.fetchone()
     return {
         "statusCode": 200,
