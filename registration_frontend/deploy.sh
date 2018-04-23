6e4e0ceeb5bc910ab75ad9b88ec5c1842d75d73d
Bryce Boe <bbzbryce@gmail.com>
2018-04-22 23:36:17 -0700

    Add registration_frontend

---
+++ registration_frontend/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./node_modules/.bin/react-scripts build \
+  && rm build/static/*/*.map \
+  && rm build/asset-manifest.json \
+  && rm build/service-worker.js \
+  && find build/static -regex '.*\.[cj]ss*' -exec sed -i '' '/^\/[/*]# sourceMappingURL/ d' {} \; \
+  && aws --profile ooo s3 cp ./build/index.html s3://oooverflow-scoreboard/registration/index.html --cache-control max-age=60 \
+  && aws --profile ooo s3 sync build/ s3://oooverflow-scoreboard/registration
