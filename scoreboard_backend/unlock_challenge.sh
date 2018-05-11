198bdd11e878510b542d37035239fc5c326c157b
Adam Doupe <adamdoupe@gmail.com>
2018-05-11 10:05:10 -0700

    simple script to unlock a challenge from the scoreboard

---
+++ scoreboard_backend/unlock_challenge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CHALLENGE_ID=$1
+
+if [ ! -e "../../challs-manager/cloned_repos/chall-$CHALLENGE_ID" ]
+then
+	echo "Error, '$CHALLENGE_ID' is not a valid challenge id"
+	exit -1
+fi
+
+sls invoke -lf challenge_open -d "{\"id\": \"$CHALLENGE_ID\" }"
