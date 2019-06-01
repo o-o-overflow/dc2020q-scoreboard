e1c4a4fb0e9fd7bc72a3dede3f9db41949346aaa
Bryce Boe <bbzbryce@gmail.com>
2019-06-01 11:33:08 -0700

    Clean up rules page

--- scoreboard_frontend/src/Rules.jsx
+++ scoreboard_frontend/src/Rules.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 function Rules() {
   return (
-    <div className="rules">
-      <h1 id="intro">Intro</h1>
-      <h1 id="rules">Rules</h1>
+    <>
+      <h2>Intro</h2>
+      <p>Some DC29 Quals specific introduction.</p>
 
+      <h2>Specifc Rules</h2>
       <ul>
         <li>
           No Denial of Service—DoS is super lame, don’t do it or you will be
@@ -18,45 +19,34 @@ function Rules() {
         <li>No automated scanning—For these challenges, do better</li>
       </ul>
 
-      <h1 id="scoring">Scoring</h1>
-
+      <h2>Scoring</h2>
       <p>
         All challenges will be adaptive scoring based on the number of solves:
         starting at 500 and decreasing from there (based on the total number of
         teams that solved the challenge).
       </p>
 
-      <h1 id="flag-format">Flag Format</h1>
-
+      <h2>Flag Format</h2>
       <p>
-        Unless otherwise noted in the challenge description, all flags will be
-        in the format:
+        All flags will be in the format: <code>OOO&#123;…&#125;</code>
       </p>
-
-      <div className="highlighter-rouge">
-        <div className="highlight">
-          <pre className="highlight">
-            <code>OOO&#123;...&#125;</code>
-          </pre>
-        </div>
-      </div>
-
       <p>
         <strong>
-          AND YOU MUST SUBMIT THE WHOLE THING, INCLUDING THE OOO&#123;…&#125;.
+          <em>
+            NOTE: You must submit the whole thing, including the{" "}
+            <code>OOO&#123;…&#125;</code>.
+          </em>
         </strong>
       </p>
 
-      <h1 id="pow">POW</h1>
-
+      <h2>Proof of Work (POW)</h2>
       <p>
-        We may implement a POW (proof of work) in front of a challenge if we
-        feel it is necessary. Specific POW, along with a client, will be
-        released at game time.
+        We may implement a POW in front of a challenge if we feel it is
+        necessary. Specific POW, along with a client, will be released at game
+        time.
       </p>
 
-      <h1 id="hints">Hints</h1>
-
+      <h2>Hints</h2>
       <p>
         Do not expect hints. Particularly if a service is already pwned, it
         would be unfair to give one team a hint when it’s already solved. If we
@@ -64,32 +54,28 @@ function Rules() {
         description and tweet about it.
       </p>
 
-      <h1>IRC/Twitter</h1>
+      <h2>Twitter and IRC</h2>
       <p>
         All game announcements will be made through our Twitter account{" "}
         <a href="https://twitter.com/oooverflow">@oooverflow</a>.
       </p>
-
       <p>
         You can hang out with us on IRC{" "}
         <a href="https://hackint.org">hackint.org</a> in #defconctf
       </p>
 
-      <h1 id="submission-delay">Flag Submission Delay</h1>
-
+      <h2>Flag Submission Delay</h2>
       <p>Flags can be submitted once every 30 seconds per challenge.</p>
 
-      <h1 id="team-size">Team Size</h1>
+      <h2>Team Size</h2>
+      <p>There is no limit on team sizes.</p>
 
-      <p>No limit on team sizes.</p>
-
-      <h1 id="disclaimer">Disclaimer</h1>
-
+      <h2>Disclaimer</h2>
       <p>
         We reserve the right to change these rules or scoring anytime before the
         competition starts.
       </p>
-    </div>
+    </>
   );
 }
 export default Rules;
