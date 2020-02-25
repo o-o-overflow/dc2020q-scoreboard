c6303e1e41c23e21d3397f0495bae35c73d2d0e5
Bryce Boe <bbzbryce@gmail.com>
2020-02-24 19:23:32 -0800

    Add registration link from countdown app

--- countdown_frontend/src/app.jsx
+++ countdown_frontend/src/app.jsx
@@ -18,6 +18,7 @@ function App() {
       <div className="container">
         <h1>Countdown to DEF CON CTF Quals</h1>
         <Counter startTime={1585353600} />
+        <p>Registration: <a href="https://register.oooverflow.io">https://register.oooverflow.io</a></p>
       </div>
     </div>
   );
