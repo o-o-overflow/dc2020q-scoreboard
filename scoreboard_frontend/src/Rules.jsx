84f3da5790b0db1535552766c9d997b3afeb9045
Yan <yans@yancomm.net>
2020-05-15 14:52:23 -0700

    add golf

--- scoreboard_frontend/src/Rules.jsx
+++ scoreboard_frontend/src/Rules.jsx
@@ -26,6 +26,25 @@ function Rules() {
         teams that solved the challenge).
       </p>
 
+      <h2>New challenge category: GOLF</h2>
+      <p>
+      Last year, we challenged you with an <a href="https://scoreboard2019.oooverflow.io/#/leaderboard/speedrun-012">entire category of speedruns</a>: bite-size problems designed for hacking races.
+      Speedrun challenges added a twist by letting the top teams dictate awarded points by beating each other to the punch.
+      </p>
+
+      <p>What if they could also dictate the difficulty?</p>
+
+      <p>
+      This year, the Order of the Overflow is excited to introduce a new style of CTF challenge: golf.
+      In a golf challenge, teams race against time to solve a challenge that's gradually degrading in difficulty.
+      The sooner they solve it, the more difficult it remains, the harder it is for other teams to catch up, and the more points it will be worth.
+      Can you keep those points out of the hands of your competition?
+      </p>
+
+      <p>
+      More information on Golf Challenges <a href="https://oooverflow.io/dc-ctf-2020-quals/">here</a>.
+      </p>
+
       <h2>Flag Format</h2>
       <p>
         All flags will be in the format: <code>OOO&#123;…&#125;</code>
@@ -54,14 +73,16 @@ function Rules() {
         description and tweet about it.
       </p>
 
-      <h2>Twitter and IRC</h2>
+      <h2>Twitter and <del>IRC</del>discord</h2>
       <p>
         All game announcements will be made through our Twitter account{" "}
-        <a href="https://twitter.com/oooverflow">@oooverflow</a>.
+        <a href="https://twitter.com/oooverflow">@oooverflow</a>
       </p>
       <p>
-        You can hang out with us on IRC{" "}
-        <a href="https://hackint.org">hackint.org</a> in #defconctf
+        Times change, and we must change with them. With DEF CON going online this year,
+        and doing so via discord, we're using discord for our idling this year.
+        You can (and should) also hang out with us on the official DEF CON discord{" "}
+        <a href="https://discord.io/ctf">discord.io/ctf</a> in the CTF category!
       </p>
 
       <h2>Flag Submission Delay</h2>
