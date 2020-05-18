807c4caaf4d7450daa1b2ca2aa39df5ac9c25c11
Bryce Boe <bbzbryce@gmail.com>
2020-05-17 23:01:38 -0700

    Create static github pages 2020 scoreboard

---
+++ precache-manifest.b8458c03881c947fc019a9fe6e84dd06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e818b6fc484cce7dc7d7cbca7ed059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e7b77cab2e61c087a61",
+    "url": "/static/css/main.334fc1f0.chunk.css"
+  },
+  {
+    "revision": "2d11bc6fa1020d972e0e",
+    "url": "/static/js/2.ba53e041.chunk.js"
+  },
+  {
+    "revision": "dea26bf88cc27ff113c9a5bd6529c3f1",
+    "url": "/static/js/2.ba53e041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7b77cab2e61c087a61",
+    "url": "/static/js/main.60db4a10.chunk.js"
+  },
+  {
+    "revision": "03aec2c04ef94616a609",
+    "url": "/static/js/runtime-main.cfcff69e.js"
+  }
+]);
